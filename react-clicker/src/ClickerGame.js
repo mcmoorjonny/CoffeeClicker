539ba467282988cc5fb7f80895ba5453c4d3509d
Padde <patick-glinka@web.de>
2018-12-17 19:33:08 +0100

    Created new files

---
+++ react-clicker/src/ClickerGame.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './ClickerGame.css';
+
+class ClickerGame extends Component {
+  render() {
+    return (<div className="ClickPanel">
+      <h1>
+        Click the Make Coffee-Button
+      </h1>
+
+    </div>);
+  }
+}
+
+export default ClickerGame;
